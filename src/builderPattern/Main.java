adf9974d731cf8aa85eff5274c4ddef9434c4ec6
koiw1202 <koiw1202@naver.com>
2024-02-22 21:48:31 +0900

    빌더패턴 메인함수 구현

---
+++ src/builderPattern/Main.java
@@ -0,0 +1,14 @@
+//package builderPattern;
+//
+///**
+// * description    :
+// * ===========================================================
+// * DATE              AUTHOR             NOTE
+// * -----------------------------------------------------------
+// * 2024-02-22        koiw1       최초 생성
+// */
+//public class Main {
+//    public static void main(String args[]) {
+//        System.out.println(Person.builder().age(1).sex("M").name("홍길동").build()) ;
+//    }
+//}
