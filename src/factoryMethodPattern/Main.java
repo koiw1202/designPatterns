220578b3ba717c5d1562d1bcbf3cae2034b78a3e
koiw1202 <koiw1202@naver.com>
2024-02-25 22:32:32 +0900

    롤백

--- src/factoryMethodPattern/Main.java
+++ src/factoryMethodPattern/Main.java
@@ -1,18 +1,18 @@
-//package factoryMethodPattern;
-//
-///**
-// * description    :
-// * ===========================================================
-// * DATE              AUTHOR             NOTE
-// * -----------------------------------------------------------
-// * 2024-02-22        koiw1       최초 생성
-// */
-//public class Main {
-//    public static void main(String args[]) {
-//        UserFactory userFactory = new NaverUserFactory();
-//        User user = userFactory.newInstance();
-//
-//        UserFactory kakaoUserFactory = new KakaoUserFactory();
-//        User kakaoUser = kakaoUserFactory.newInstance();
-//    }
-//}
+package factoryMethodPattern;
+
+/**
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-02-22        koiw1       최초 생성
+ */
+public class Main {
+    public static void main(String args[]) {
+        UserFactory userFactory = new NaverUserFactory();
+        User user = userFactory.newInstance();
+
+        UserFactory kakaoUserFactory = new KakaoUserFactory();
+        User kakaoUser = kakaoUserFactory.newInstance();
+    }
+}
--- src/prototypePattern/Main.java
+++ src/prototypePattern/Main.java
@@ -1,30 +1,30 @@
-//package prototypePattern;
-//
-//import java.util.List;
-//
-///**
-// * description    :
-// * ===========================================================
-// * DATE              AUTHOR             NOTE
-// * -----------------------------------------------------------
-// * 2024-02-22        koiw1       최초 생성
-// */
-//public class Main {
-//    public static void main(String args[]) throws CloneNotSupportedException {
-//        Employees emps = new Employees();
-//        emps.loadData(); // Ann, John, Methew...
-//
-//        Employees emps1 = (Employees) emps.clone();
-//        Employees emps2 = (Employees) emps.clone();
-//
-//        List< String > list1 = emps1.getEmpList();
-//        list1.add("Peter");
-//
-//        List < String > list2 = emps2.getEmpList();
-//        list2.remove("John");
-//
-//        System.out.println("emps: " + emps.getEmpList());
-//        System.out.println("emps1: " + list1);
-//        System.out.println("emps2: " + list2);
-//    }
-//}
+package prototypePattern;
+
+import java.util.List;
+
+/**
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-02-22        koiw1       최초 생성
+ */
+public class Main {
+    public static void main(String args[]) throws CloneNotSupportedException {
+        Employees emps = new Employees();
+        emps.loadData(); // Ann, John, Methew...
+
+        Employees emps1 = (Employees) emps.clone();
+        Employees emps2 = (Employees) emps.clone();
+
+        List< String > list1 = emps1.getEmpList();
+        list1.add("Peter");
+
+        List < String > list2 = emps2.getEmpList();
+        list2.remove("John");
+
+        System.out.println("emps: " + emps.getEmpList());
+        System.out.println("emps1: " + list1);
+        System.out.println("emps2: " + list2);
+    }
+}
